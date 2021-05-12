59d0ba5346438020faad14eaad1e4116f7861397
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-05-12 17:50:21 +0200

    Struttura navbar + inserimento comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
+
   </head>
   <body>
 
   @include('re_use._header')
 
   <main>
+    <section>
+      <div class="container">
+        <div class="jumbo">
+          <div class="content-jumbo">
 
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="current-series">
+          <div class="content-current-series">
+            @foreach ($comics as $comic)
+            <div class="card-series">
+              <div class="content-card-series">
+                <img src="{{ $comic['thumb'] }}" alt="">
+                <h3> {{ $comic['series'] }} </h3>
+              </div>
+            </div>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 
   @include('re_use._footer')
--- resources/views/re_use/_header.blade.php
+++ resources/views/re_use/_header.blade.php
@@ -12,28 +12,30 @@
       <div class="menu-navbar">
         <div class="content-menu-navbar">
           <div class="right-menu-navbar">
-
+            <div class="content-right-menu-navbar">
+              <img src="{{ asset('img/dc-logo.png') }}" alt="">
+            </div>
           </div>
           <div class="center-menu-navbar">
             <div class="content-center-menu-navbar">
               <ul>
                 <li><a href=""><img src="" alt=""></a></li>
-                <li><a href=""></a>characters</li>
-                <li><a href=""></a>comics</li>
-                <li><a href=""></a>movies</li>
-                <li><a href=""></a>tv</li>
-                <li><a href=""></a>games</li>
-                <li><a href=""></a>collectibles</li>
-                <li><a href=""></a>videos</li>
-                <li><a href=""></a>fans</li>
-                <li><a href=""></a>news</li>
-                <li><a href=""></a>shop</li>
+                <li><a href="">characters</a></li>
+                <li><a href="">comics</a></li>
+                <li><a href="">movies</a></li>
+                <li><a href="">tv</a></li>
+                <li><a href="">games</a></li>
+                <li><a href="">collectibles</a></li>
+                <li><a href="">videos</a></li>
+                <li><a href="">fans</a></li>
+                <li><a href="">news</a></li>
+                <li><a href="">shop</a></li>
               </ul>
             </div>
           </div>
           <div class="left-menu-navbar">
-            <div class="content-left-navbar">
-              <input type="text" name="" value="">
+            <div class="content-left-menu-navbar">
+              <input type="text" name="" value="" placeholder="Search">
             </div>
           </div>
         </div>
