f3623627272deee5560fedfc48440ad2251b2f4c
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-05-12 16:37:38 +0200

    Inserimento dati, immagini e creazione partials

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  $database = config('db_dischi');
+
+  return view('home')->with('comics', $database);
 });
